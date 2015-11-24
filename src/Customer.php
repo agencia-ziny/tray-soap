7f8835cb65d6152031948b0da87a017750a27d06
Álvaro Guimarães <alvaroguimaraes@gmail.com>
2015-11-24 16:13:57 -0200

    Customer::getFromToday method.

--- src/Customer.php
+++ src/Customer.php
@@ -4,7 +4,7 @@ namespace Aguimaraes\TraySoap;
 
 class Customer extends Client
 {
-    public function get()
+    public function get(array $params)
     {
         return $this->soapClient->__soapCall(
             'fWSImportaClientes',
@@ -12,7 +12,18 @@ class Customer extends Client
                 'pid_loja' => $this->shopId,
                 'plogin' => $this->login,
                 'psenha' => $this->pwd
-            )
+            ) + $params
         );
+    }
+
+    public function getFromToday()
+    {
+        $params = array(
+            'data_inicial' => date('Y-m-d'),
+            'data_final' => date('Y-m-d'),
+            'hora_inicial' => '0000',
+            'hora_final' => '2359'
+        );
+        return $this->get($params);
     }
 }
