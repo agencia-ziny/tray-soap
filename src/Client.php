c859d317638303c7728eaca1cbf7f54ca2c86dcf
Álvaro Guimarães <alvaroguimaraes@gmail.com>
2015-11-25 10:16:05 -0200

    Specific data type for soap call param and docblock

--- src/Client.php
+++ src/Client.php
@@ -76,8 +76,13 @@ abstract class Client
         $this->pwd = $pwd;
         return $this;
     }
-
-    public function __soapCall($function, $params)
+    /**
+     * Make a SOAP CALL with default auth params
+     * @param  string $function
+     * @param  array $params
+     * @return array
+     */
+    public function __soapCall($function, array $params)
     {
         if (empty($this->shopId) || empty($this->login) || empty($this->pwd)) {
             throw new \Exception('Missing auth params');
