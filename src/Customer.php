52fcab559cf7e3dbd364ff5c116853676a69f5b0
Álvaro Guimarães <alvaroguimaraes@gmail.com>
2015-11-25 08:55:05 -0200

    Removed auth params and common logic methods.

--- src/Customer.php
+++ src/Customer.php
@@ -4,26 +4,10 @@ namespace Aguimaraes\TraySoap;
 
 class Customer extends Client
 {
+    use ClientTrait;
+    
     public function get(array $params)
     {
-        return $this->soapClient->__soapCall(
-            'fWSImportaClientes',
-            array(
-                'pid_loja' => $this->shopId,
-                'plogin' => $this->login,
-                'psenha' => $this->pwd
-            ) + $params
-        );
-    }
-
-    public function getFromToday()
-    {
-        $params = array(
-            'data_inicial' => date('Y-m-d'),
-            'data_final' => date('Y-m-d'),
-            'hora_inicial' => '0000',
-            'hora_final' => '2359'
-        );
-        return $this->get($params);
+        return $this->soapClient->__soapCall('fWSImportaClientes', $params);
     }
 }
