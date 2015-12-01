7ba3f1425714248973d5437e100b0b525ea7f2b7
Álvaro Guimarães <alvaroguimaraes@gmail.com>
2015-12-01 15:24:26 -0200

    new array definition format and DateTime correct namespace

--- src/ClientTrait.php
+++ src/ClientTrait.php
@@ -6,12 +6,12 @@ trait ClientTrait
 {
     public function getFromToday()
     {
-        return $this->get(array(
+        return $this->get([
             'data_inicial' => date('Y-m-d'),
             'data_final' => date('Y-m-d'),
             'hora_inicial' => '0000',
             'hora_final' => '2359'
-        ));
+        ]);
     }
 
     public function getFromYesterday()
@@ -19,12 +19,12 @@ trait ClientTrait
         $obj = new \DateTime();
         $obj->modify('yesterday');
         $yesterday = $obj->format('Y-m-d');
-        return $this->get(array(
+        return $this->get([
             'data_inicial' => $yesterday,
             'data_final' => $yesterday,
             'hora_inicial' => '0000',
             'hora_final' => '2359'
-        ));
+        ]);
     }
 
     public function getFromCurrentWeek()
@@ -32,12 +32,12 @@ trait ClientTrait
         $obj = new \DateTime();
         $obj->modify('last sunday');
         $lastSunday = $obj->format('Y-m-d');
-        return $this->get(array(
+        return $this->get([
             'data_inicial' => $lastSunday,
             'data_final' => date('Y-m-d'),
             'hora_inicial' => '0000',
             'hora_final' => '2359'
-        ));
+        ]);
     }
     /**
      * Get from specific date and time
@@ -45,13 +45,13 @@ trait ClientTrait
      * @param  DateTime|null $toDate
      * @return array
      */
-    public function getFromSpecificDateAndTime(DateTime $fromDate, DateTime $toDate = null)
+    public function getFromSpecificDateAndTime(\DateTime $fromDate, \DateTime $toDate = null)
     {
-        return $this->get(array(
+        return $this->get([
             'data_inicial' => $fromDate->format('Y-m-d'),
             'data_final' => empty($toDate) ? date('Y-m-d') : $toDate->format('Y-m-d'),
             'hora_inicial' => '0000',
             'hora_final' => '2359'
-        ));
+        ]);
     }
 }
