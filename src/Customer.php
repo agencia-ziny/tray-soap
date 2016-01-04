00f3b6af59128be8de2ca34aa680c9181bf70291
Rafael Henrique Berro <rhberro@gmail.com>
2016-01-04 09:52:07 -0200

    Added sync and get methods.

--- src/Customer.php
+++ src/Customer.php
@@ -4,8 +4,26 @@ namespace Aguimaraes\TraySoap;
 
 class Customer extends Client
 {
-    public function get(array $params)
+    public function get($id = null)
     {
-        return $this->call('fWSImportaClientes', $params);
+        return is_numeric($id) ?
+            $this->getById($id) :
+            $this->getAll();
+    }
+
+    public function getAll(array $params = array())
+    {
+        return $this->call(
+            'fWSImportaClientes',
+            $params
+        );
+    }
+
+    public function getById($id)
+    {
+        return $this->call(
+            'fWSImportaClientePorId',
+            ['id_cliente' => $id]
+        );
     }
 }
--- src/Order.php
+++ src/Order.php
@@ -4,8 +4,39 @@ namespace Aguimaraes\TraySoap;
 
 class Order extends Client
 {
-    public function get(array $params)
+    public function get($id = null)
     {
-        return $this->call('fWSImportaPedidos', $params);
+        return is_numeric($id) ?
+            $this->getById($id) :
+            $this->getAll();
+    }
+
+    public function getAll(array $params = array())
+    {
+        return $this->call(
+            'fWSImportaPedidos',
+            $params
+        );
+    }
+
+    public function getById($id)
+    {
+        return $this->call(
+            'fWSImportaPedidoPorId',
+            [
+                'id_pedido' => $id
+            ]
+        );
+    }
+
+    public function sync($id)
+    {
+        return $this->call(
+            'fWSAtualizaListaToDo',
+            [
+                'entidade' => 'pedidos',
+                'id' => $id
+            ]
+        );
     }
 }
